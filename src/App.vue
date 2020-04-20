beb341196c4b3b310decb8ef266fe629304df89c
wangyuge994 <1317876468@qq.com>
2020-04-20 16:09:44 +0800

    用户登录-创建组件并配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h1>黑马头条发布系统</h1>
     <!-- 路由出口 -->
     <router-view />
   </div>
---
+++ src/views/login/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="login-container">登录页面</div>
+</template>
+
+<script>
+export default {}
+</script>

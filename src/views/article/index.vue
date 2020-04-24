c3e5109dcd4e31ee7d4fd93ffc748e4e1b052ca1
wangyuge994 <1317876468@qq.com>
2020-04-24 10:29:15 +0800

    内容管理-封面处理

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -63,6 +63,15 @@
         <el-table-column
           prop="date"
           label="封面">
+          <template slot-scope="scope">
+            <img
+            v-if="scope.row.cover.images[0]"
+            class="article-cover"
+            :src="scope.row.cover.images[0]" alt="">
+            <img
+            v-else class="article-cover"
+            src="./no-cover.gif" alt="">
+          </template>
         </el-table-column>
         <el-table-column
           prop="title"
@@ -72,11 +81,12 @@
           label="状态">
           <!-- 如果需要在自定义列模板中获取当前遍历项数据，那么就在 template 上声明 slot-scope="scope" -->
           <template slot-scope="scope">
-            <el-tag v-if="scope.row.status === 0" type="warning">草稿</el-tag>
+            <el-tag :type="articleStatus[scope.row.status].type">{{ articleStatus[scope.row.status].text }}</el-tag>
+            <!-- <el-tag v-if="scope.row.status === 0" type="warning">草稿</el-tag>
             <el-tag v-else-if="scope.row.status === 1">待审核</el-tag>
             <el-tag v-else-if="scope.row.status === 2" type="success">审核通过</el-tag>
             <el-tag v-else-if="scope.row.status === 3" type="danger">审核失败</el-tag>
-            <el-tag v-else-if="scope.row.status === 4" type="info">已删除</el-tag>
+            <el-tag v-else-if="scope.row.status === 4" type="info">已删除</el-tag> -->
           </template>
         </el-table-column>
         <el-table-column
@@ -134,7 +144,14 @@ export default {
         resource: '',
         desc: ''
       },
-      articles: [] // 文章数据列表
+      articles: [], // 文章数据列表
+      articleStatus: [
+        { status: 0, text: '草稿', type: 'info' }, // 0
+        { status: 1, text: '待审核', type: '' }, // 1
+        { status: 2, text: '审核通过', type: 'success' }, // 2
+        { status: 3, text: '审核失败', type: 'warning' }, // 3
+        { status: 4, text: '已删除', type: 'danger' } // 4
+      ]
     }
   },
   computed: {},
@@ -164,4 +181,10 @@ export default {
 
 .list-table {
   margin-bottom: 20px;
-}</style>
+}
+
+.article-cover {
+  width: 100px;
+  background-size: cover;
+}
+</style>

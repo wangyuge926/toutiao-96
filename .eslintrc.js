3725083a466c75fd720740145b7d2f037d548bea
wangyuge994 <1317876468@qq.com>
2020-04-29 10:21:00 +0800

    自定义ESLint代码校验规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+// ESLint 代码规范校验工具的配置文件
+// 注意: 该文件如果修改必须重启才能生效
+
 module.exports = {
   root: true,
   env: {
@@ -12,6 +15,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off' // 关闭这个规则验证
   }
 }
